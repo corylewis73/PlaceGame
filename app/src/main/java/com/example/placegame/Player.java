a27c274d37c1cd2446ed27d4ad9ae5cf75d2af84
corylewis73 <corylewis73@gmail.com>
2022-05-08 23:35:18 -0400

    Added 3 deck buttons for potential types of tiles each player can place.

---
+++ app/src/main/java/com/example/placegame/Player.java
@@ -0,0 +1,11 @@
+package com.example.placegame;
+
+public class Player {
+    public boolean isTurn = false;
+    public String playerColor;
+
+    // Number of uses for each type of tile in the deck
+    public int regularUses = 10;
+    public int horizontalUses = 1;
+    public int verticalUses = 1;
+}
