5ee6bd4206d4a4cf40b0024130b57a2bd0e6c5c0
joshuas4 <joshuas3@umbc.edu>
2022-05-19 18:06:13 -0400

    Adds in launcher activity and xml files

--- app/src/main/java/com/example/placegame/Launcher.java
+++ app/src/main/java/com/example/placegame/Launcher.java
@@ -3,12 +3,41 @@ package com.example.placegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Launcher extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class Launcher extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView titleText;
+    private TextView instructionsText;
+
+    private Button singlePlayer;
+    private Button multiplayerHost;
+    private Button multiplayerClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        singlePlayer = (Button) findViewById(R.id.buttonSP);
+        multiplayerHost = (Button) findViewById(R.id.buttonMPH);
+        multiplayerClient = (Button) findViewById(R.id.buttonMPP);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.buttonSP):
+                //implement here.
+                break;
+            case (R.id.buttonMPH):
+                break;
+            case (R.id.buttonMPP):
+                break;
+        }
     }
 }
