c6a267fdbad05e72983d8be464018993977a2cd4
joshuas4 <joshuas3@umbc.edu>
2022-05-22 17:25:38 -0400

    Setting up initial stuff for multiplayer

---
+++ app/src/main/java/com/example/placegame/MultiplayerPage.java
@@ -0,0 +1,14 @@
+package com.example.placegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiplayerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplayer_page);
+    }
+}
